1cd6986d2c891f7aede08c35e1fb99f7fd6e7036
Iskam31 <125703159+Iskam31@users.noreply.github.com>
2023-04-18 23:36:41 +0300

    Delete client_kb.py

--- client_kb.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-
-b1 = KeyboardButton('/My_case')
-b2 = KeyboardButton('/All_case')
-
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-
-kb_client.row(b1,b2)
